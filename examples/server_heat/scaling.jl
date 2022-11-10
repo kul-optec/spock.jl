6ef75144392c71aa3873d04ce6c601714cbb0570
Alexander <alexander.bodard@student.kuleuven.be>
2022-11-10 18:30:46 +0100

    Add a maximal execution time to the open-loop simulation

--- examples/server_heat/scaling.jl
+++ examples/server_heat/scaling.jl
@@ -8,29 +8,27 @@ pgfplotsx()
 
 M = 1
 N_max = 12
-N_ref_max = 11
-N_low = 7
 # Dimensions of state and input vectors
 nx = 50
 x0 = [i <= 2 ? .1 : .1 for i = 1:nx]
 d = 2
 TOL = 1e-3
 
+t_max = 100
+
 model_timings = zeros(N_max - 2)
 mosek_timings = zeros(N_max - 2)
 gurobi_timings = zeros(N_max - 2)
-ipopt_timings = zeros(N_ref_max - 2)
-sedumi_timings = zeros(N_low - 2)
-cosmo_timings = zeros(N_ref_max - 2)
+ipopt_timings = zeros(N_max - 2)
+sedumi_timings = zeros(N_max - 2)
+cosmo_timings = zeros(N_max - 2)
 
 for m = 1:M
-  for N = 3:max(N_ref_max, N_max)
+  for N = 3:N_max
 
     scen_tree, cost, dynamics, rms = get_server_heat_specs(N, nx, d)
 
-    if N <= N_max
-      model = spock.build_model(scen_tree, cost, dynamics, rms, spock.SolverOptions(spock.L_IMPLICIT, spock.SP))
-    end
+    model = spock.build_model(scen_tree, cost, dynamics, rms, spock.SolverOptions(spock.L_IMPLICIT, spock.SP))
 
     model_mosek = spock.build_model_mosek(scen_tree, cost, dynamics, rms)
     set_optimizer_attribute(model_mosek, "MSK_DPAR_INTPNT_TOL_REL_GAP", TOL)
@@ -54,21 +52,26 @@ for m = 1:M
     ###  Solution
     ##########################################
 
-    if N <= N_max
+    if maximum(model_timings) <= t_max
       model_timings[N - 2] += @elapsed spock.solve_model!(model, x0, tol=TOL)
+    end
+    if maximum(mosek_timings) <= t_max
       mosek_timings[N - 2] += @elapsed spock.solve_model(model_mosek, x0)
+    end
+    if maximum(gurobi_timings) <= t_max
       gurobi_timings[N - 2] += @elapsed spock.solve_model(model_gurobi, x0)
     end
-    if N <= N_ref_max
-      # gurobi_timings[N - 2] += @elapsed spock.solve_model(model_gurobi, x0)
+    if maximum(ipopt_timings) <= t_max
       ipopt_timings[N - 2] += @elapsed spock.solve_model(model_ipopt, x0)
+    end
+    if maximum(cosmo_timings) <= t_max
       cosmo_timings[N - 2] += @elapsed spock.solve_model(model_cosmo, x0)
     end
-    if N <= N_low
+    if maximum(sedumi_timings) <= t_max
       sedumi_timings[N - 2] += @elapsed spock.solve_model(model_sedumi, x0)
     end
 
-    println("SPOCK: $(model.solver_state.z[model.solver_state_internal.s_inds[1]]), MOSEK: $(value(model_mosek[:s][1]))")
+    # println("SPOCK: $(model.solver_state.z[model.solver_state_internal.s_inds[1]]), MOSEK: $(value(model_mosek[:s][1]))")
 
   end
 
@@ -87,13 +90,19 @@ fig = plot(
   legend = true
 )
 
-plot!(3:N_max, model_timings, color=:red, yaxis=:log, labels=["SPOCK"])
-plot!(3:N_max, mosek_timings, color=:blue, yaxis=:log, labels=["MOSEK"])
-plot!(3:N_max, gurobi_timings, color=:green, yaxis=:log, labels=["GUROBI"])
-plot!(3:N_ref_max, ipopt_timings, color=:purple, yaxis=:log, labels=["IPOPT"])
-plot!(3:N_low, sedumi_timings, color=:orange, yaxis=:log, labels=["SEDUMI"])
-plot!(3:N_ref_max, cosmo_timings, color=:black, yaxis=:log, labels=["COSMO"])
+model_timings = filter(>(0.), model_timings)
+mosek_timings = filter(>(0.), mosek_timings)
+gurobi_timings = filter(>(0.), gurobi_timings)
+ipopt_timings = filter(>(0.), ipopt_timings)
+sedumi_timings = filter(>(0.), sedumi_timings)
+cosmo_timings = filter(>(0.), cosmo_timings)
+
+plot!(3:length(model_timings) + 2, model_timings, color=:red, yaxis=:log, labels=["SPOCK"])
+plot!(3:length(mosek_timings) + 2, mosek_timings, color=:blue, yaxis=:log, labels=["MOSEK"])
+plot!(3:length(gurobi_timings) + 2, gurobi_timings, color=:green, yaxis=:log, labels=["GUROBI"])
+plot!(3:length(ipopt_timings) + 2, ipopt_timings, color=:purple, yaxis=:log, labels=["IPOPT"])
+plot!(3:length(sedumi_timings) + 2, sedumi_timings, color=:orange, yaxis=:log, labels=["SEDUMI"])
+plot!(3:length(cosmo_timings) + 2, cosmo_timings, color=:black, yaxis=:log, labels=["COSMO"])
 
 savefig("examples/server_heat/output/scaling.pdf")
-
 savefig("examples/server_heat/output/scaling.tikz")
