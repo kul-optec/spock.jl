3d5564084fd94a1d582eb39f108564b4a836a81e
Alexander <alexander.bodard@student.kuleuven.be>
2022-11-18 16:01:11 +0100

    Update cold start plot

--- examples/server_heat/scaling.jl
+++ examples/server_heat/scaling.jl
@@ -14,7 +14,7 @@ x0 = [i <= 2 ? .1 : .1 for i = 1:nx]
 d = 2
 TOL = 1e-3
 
-t_max = 300
+t_max = 150
 
 model_timings = zeros(N_max - 2)
 mosek_timings = zeros(N_max - 2)
@@ -35,18 +35,18 @@ for m = 1:M
     set_optimizer_attribute(model_mosek, "MSK_DPAR_INTPNT_CO_TOL_REL_GAP", TOL)
     set_optimizer_attribute(model_mosek, "MSK_DPAR_INTPNT_QO_TOL_REL_GAP", TOL)
 
-    # model_gurobi = spock.build_model_gurobi(scen_tree, cost, dynamics, rms, constraints)
-    # set_optimizer_attribute(model_gurobi, "FeasibilityTol", TOL)
-    # set_optimizer_attribute(model_gurobi, "OptimalityTol", TOL)
+    model_gurobi = spock.build_model_gurobi(scen_tree, cost, dynamics, rms, constraints)
+    set_optimizer_attribute(model_gurobi, "FeasibilityTol", TOL)
+    set_optimizer_attribute(model_gurobi, "OptimalityTol", TOL)
 
-    # model_ipopt = spock.build_model_ipopt(scen_tree, cost, dynamics, rms, constraints)
-    # set_optimizer_attribute(model_ipopt, "tol", TOL)
+    model_ipopt = spock.build_model_ipopt(scen_tree, cost, dynamics, rms, constraints)
+    set_optimizer_attribute(model_ipopt, "tol", TOL)
 
-    # model_sedumi = spock.build_model_sedumi(scen_tree, cost, dynamics, rms, constraints)
+    model_sedumi = spock.build_model_sedumi(scen_tree, cost, dynamics, rms, constraints)
 
-    # model_cosmo = spock.build_model_cosmo(scen_tree, cost, dynamics, rms, constraints)
-    # set_optimizer_attribute(model_cosmo, "eps_abs", TOL)
-    # set_optimizer_attribute(model_cosmo, "eps_rel", TOL)
+    model_cosmo = spock.build_model_cosmo(scen_tree, cost, dynamics, rms, constraints)
+    set_optimizer_attribute(model_cosmo, "eps_abs", TOL)
+    set_optimizer_attribute(model_cosmo, "eps_rel", TOL)
 
     ##########################################
     ###  Solution
@@ -60,18 +60,18 @@ for m = 1:M
     if maximum(mosek_timings) <= t_max
       mosek_timings[N - 2] += @elapsed spock.solve_model(model_mosek, x0)
     end
-    # if maximum(gurobi_timings) <= t_max
-    #   gurobi_timings[N - 2] += @elapsed spock.solve_model(model_gurobi, x0)
-    # end
-    # if maximum(ipopt_timings) <= t_max
-    #   ipopt_timings[N - 2] += @elapsed spock.solve_model(model_ipopt, x0)
-    # end
-    # if maximum(cosmo_timings) <= t_max
-    #   cosmo_timings[N - 2] += @elapsed spock.solve_model(model_cosmo, x0)
-    # end
-    # if maximum(sedumi_timings) <= t_max
-    #   sedumi_timings[N - 2] += @elapsed spock.solve_model(model_sedumi, x0)
-    # end
+    if maximum(gurobi_timings) <= t_max
+      gurobi_timings[N - 2] += @elapsed spock.solve_model(model_gurobi, x0)
+    end
+    if maximum(ipopt_timings) <= t_max && N <= 12
+      ipopt_timings[N - 2] += @elapsed spock.solve_model(model_ipopt, x0)
+    end
+    if maximum(cosmo_timings) <= t_max
+      cosmo_timings[N - 2] += @elapsed spock.solve_model(model_cosmo, x0)
+    end
+    if maximum(sedumi_timings) <= t_max
+      sedumi_timings[N - 2] += @elapsed spock.solve_model(model_sedumi, x0)
+    end
 
     # println("SPOCK: $(model.solver_state.z[model.solver_state_internal.s_inds[1]]), MOSEK: $(value(model_mosek[:s][1]))")
 
@@ -92,19 +92,19 @@ fig = plot(
   legend = true
 )
 
-# model_timings = filter(>(0.), model_timings)
-# mosek_timings = filter(>(0.), mosek_timings)
-# gurobi_timings = filter(>(0.), gurobi_timings)
-# ipopt_timings = filter(>(0.), ipopt_timings)
-# sedumi_timings = filter(>(0.), sedumi_timings)
-# cosmo_timings = filter(>(0.), cosmo_timings)
+model_timings = filter(>(0.), model_timings)
+mosek_timings = filter(>(0.), mosek_timings)
+gurobi_timings = filter(>(0.), gurobi_timings)
+ipopt_timings = filter(>(0.), ipopt_timings)
+sedumi_timings = filter(>(0.), sedumi_timings)
+cosmo_timings = filter(>(0.), cosmo_timings)
 
 plot!(3:length(model_timings) + 2, model_timings, color=:red, yaxis=:log, labels=["SPOCK"])
 plot!(3:length(mosek_timings) + 2, mosek_timings, color=:blue, yaxis=:log, labels=["MOSEK"])
-# plot!(3:length(gurobi_timings) + 2, gurobi_timings, color=:green, yaxis=:log, labels=["GUROBI"])
-# plot!(3:length(ipopt_timings) + 2, ipopt_timings, color=:purple, yaxis=:log, labels=["IPOPT"])
-# plot!(3:length(sedumi_timings) + 2, sedumi_timings, color=:orange, yaxis=:log, labels=["SEDUMI"])
-# plot!(3:length(cosmo_timings) + 2, cosmo_timings, color=:black, yaxis=:log, labels=["COSMO"])
+plot!(3:length(gurobi_timings) + 2, gurobi_timings, color=:green, yaxis=:log, labels=["GUROBI"])
+plot!(3:length(ipopt_timings) + 2, ipopt_timings, color=:purple, yaxis=:log, labels=["IPOPT"])
+plot!(3:length(sedumi_timings) + 2, sedumi_timings, color=:orange, yaxis=:log, labels=["SEDUMI"])
+plot!(3:length(cosmo_timings) + 2, cosmo_timings, color=:black, yaxis=:log, labels=["COSMO"])
 
 savefig("examples/server_heat/output/scaling.pdf")
 savefig("examples/server_heat/output/scaling.tikz")
