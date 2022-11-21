4ce966ba62d1e5596f7b147fe179422432204487
Documenter.jl <documenter@juliadocs.github.io>
2022-11-21 10:55:13 +0000

    build based on e907269

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"examples/#Examples","page":"Examples","title":"Examples","text":"","category":"section"},{"location":"examples/","page":"Examples","title":"Examples","text":"The examples folder contains examples on how to use the spock.jl solver.","category":"page"},{"location":"","page":"Home","title":"Home","text":"This is the spock.jl home page.","category":"page"}]
+}
